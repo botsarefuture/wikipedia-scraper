18279812674e46121bee5ccabb2f18cf8586fca4
botsarefuture <101255718+botsarefuture@users.noreply.github.com>
2022-10-12 12:12:14 +0300

    Update index.py

--- index.py
+++ index.py
@@ -7,6 +7,7 @@ CONS = []
 LANG = "fi"
 FROM = "A"
 TO = "Ö"
+NUM_LIMIT = 5000
 f = open("datas.txt", "r")
 data = f.read()
 f.close()
@@ -25,7 +26,7 @@ CON_KEY = ""
 while True:
     try:
         if not CON_KEY == "":
-            url = f"https://{LANG}.wikipedia.org/w/api.php?action=query&list=allpages&apfrom={FROM}&apto={TO}&format=json&apcontinue={CON_KEY}&aplimit=10000"
+            url = f"https://{LANG}.wikipedia.org/w/api.php?action=query&list=allpages&apfrom={FROM}&apto={TO}&format=json&apcontinue={CON_KEY}&aplimit={NUM_LIMIT}"
 
             payload={}
             headers = {
@@ -34,21 +35,22 @@ while True:
 
             response = requests.request("GET", url, headers=headers, data=payload)
             CON_KEY = response.json()["continue"]["apcontinue"]
-            NUM_ITEMS += int(count(response.json()["query"]["allpages"]))
+            #NUM_ITEMS += int(count(response.json()["query"]["allpages"]))
             print(f'GETTED {count(response.json()["query"]["allpages"])} ITEMS')
             print(f"TOTAL ITEMS COUNT: {NUM_ITEMS}")
-            time.sleep(2)
+            time.sleep(0.5)
             datas_add = """"""
             for cat in response.json()["query"]["allpages"]:
                 if not cat["title"] in READY:
                     datas_add += cat["title"] + "\n"
                     READY.append(cat["title"])
+                    NUM_ITEMS += 1
                     print(cat["title"])
             f = open("datas.txt", "a")
             f.write(datas_add)
             f.close()
         else:
-            url = f"https://{LANG}.wikipedia.org/w/api.php?action=query&list=allpages&apfrom={FROM}&apto={TO}&format=json&aplimit=20"
+            url = f"https://{LANG}.wikipedia.org/w/api.php?action=query&list=allpages&apfrom={FROM}&apto={TO}&format=json&aplimit={NUM_LIMIT}"
             payload={}
             headers = {
         'Cookie': 'GeoIP=US:VA:Ashburn:39.05:-77.49:v4; WMF-Last-Access-Global=10-Oct-2022; WMF-Last-Access=10-Oct-2022'
@@ -56,15 +58,16 @@ while True:
 
             response = requests.request("GET", url, headers=headers, data=payload)
             CON_KEY = response.json()["continue"]["apcontinue"]
-            NUM_ITEMS += int(count(response.json()["query"]["allpages"]))
+            #NUM_ITEMS += int(count(response.json()["query"]["allpages"]))
             print(f'GETTED {count(response.json()["query"]["allpages"])} ITEMS')
             print(f"TOTAL ITEMS COUNT: {NUM_ITEMS}")
-            time.sleep(2)
+            time.sleep(0.5)
             datas_add = """"""
             for cat in response.json()["query"]["allpages"]:
                 if not cat["title"] in READY:
                     datas_add += cat["title"] + "\n"
                     READY.append(cat["title"])
+                    NUM_ITEMS += 1
                     print(cat["title"])
             f = open("datas.txt", "a")
             f.write(datas_add)
