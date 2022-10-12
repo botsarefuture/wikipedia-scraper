6ea068904143614342b3fac5e779bfb96ca49f75
botsarefuture <101255718+botsarefuture@users.noreply.github.com>
2022-10-12 09:49:11 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 
 import requests
 import time
+
 READY = []
 CONS = []
 LANG = "fi"
@@ -10,22 +11,21 @@ f = open("datas.txt", "r")
 data = f.read()
 f.close()
 data = data.split("\n")
+NUM_ITEMS = 0
 for data in data:
+    NUM_ITEMS += 1
     READY.append(data)
-def start(READY, request, cont):
-    response = request()
-    f = open("datas.txt", "a")
-    for cat in response.json()["query"]["allpages"]:
-        f.write(cat["title"] + "\n")
-        READY.append(cat["title"])
-        print(cat["title"])
-    f.close()
-    cont(response)
-
-try:
-    def request():
-        try:
-            url = f"https://{LANG}.wikipedia.org/w/api.php?action=query&list=allpages&apfrom={FROM}&apto={TO}&format=json&aplimit=20"
+def count(lista):
+    count1 = 0
+    for list1 in lista:
+        count1 += 1
+    return count1
+times = 0
+CON_KEY = ""
+while True:
+    try:
+        if not CON_KEY == "":
+            url = f"https://{LANG}.wikipedia.org/w/api.php?action=query&list=allpages&apfrom={FROM}&apto={TO}&format=json&apcontinue={CON_KEY}&aplimit=10000"
 
             payload={}
             headers = {
@@ -33,71 +33,43 @@ try:
         }
 
             response = requests.request("GET", url, headers=headers, data=payload)
-            return response
-        except UnicodeDecodeError:
-            print("Unicode")
-            return request()
-    def req_continue(con_ey):
-        try:
-            if not con_ey in CONS:
-                CONS.append(con_ey)
-                url = f"https://{LANG}.wikipedia.org/w/api.php?action=query&list=allpages&apfrom={FROM}&apto={TO}&format=json&apcontinue={con_ey}&aplimit=1000000"
-
-                payload={}
-                headers = {
-            'Cookie': 'GeoIP=US:VA:Ashburn:39.05:-77.49:v4; WMF-Last-Access-Global=10-Oct-2022; WMF-Last-Access=10-Oct-2022'
-            }
-
-                response = requests.request("GET", url, headers=headers, data=payload)
-                f = open("cat.txt", "w")
-                f.write(response.json()["continue"]["apcontinue"])
-                f.close()
-                print("GETTED DATA")
-                time.sleep(2)
-                return response
-            return request()
-        except UnicodeDecodeError:
-            print("UNi")
-            return request()
-    def cont(response):
-        if type(response) == str:
-            try:
-                response = req_continue(response)
-                datas_add = """"""
-                
-                for cat in response.json()["query"]["allpages"]:
-                    if not cat["title"] in READY:
-                        datas_add += cat["title"] + "\n"
-                        READY.append(cat["title"])
-                        print("ready")
-                f = open("datas.txt", "a")
-                f.write(datas_add)
-                f.close()
-                cont(response)
-            except UnicodeEncodeError:
-                print("UNICODE")
-                cont(response)
+            CON_KEY = response.json()["continue"]["apcontinue"]
+            NUM_ITEMS += int(count(response.json()["query"]["allpages"]))
+            print(f'GETTED {count(response.json()["query"]["allpages"])} ITEMS')
+            print(f"TOTAL ITEMS COUNT: {NUM_ITEMS}")
+            time.sleep(2)
+            datas_add = """"""
+            for cat in response.json()["query"]["allpages"]:
+                if not cat["title"] in READY:
+                    datas_add += cat["title"] + "\n"
+                    READY.append(cat["title"])
+                    print(cat["title"])
+            f = open("datas.txt", "a")
+            f.write(datas_add)
+            f.close()
         else:
-            try:
-                response = req_continue(response.json()["continue"]["apcontinue"])
-                datas_add = """"""
-                for cat in response.json()["query"]["allpages"]:
-                    if not cat["title"] in READY:
-                        datas_add += cat["title"] + "\n"
-                        READY.append(cat["title"])
-                        print(cat["title"])
-                f = open("datas.txt", "a")
-                f.write(datas_add)
-                f.close()
-                cont(response)
-            except UnicodeEncodeError:
-                print("UNICODE")
-                cont(response)
+            url = f"https://{LANG}.wikipedia.org/w/api.php?action=query&list=allpages&apfrom={FROM}&apto={TO}&format=json&aplimit=20"
+            payload={}
+            headers = {
+        'Cookie': 'GeoIP=US:VA:Ashburn:39.05:-77.49:v4; WMF-Last-Access-Global=10-Oct-2022; WMF-Last-Access=10-Oct-2022'
+        }
 
-    start(READY, request, cont)
-except RecursionError:
-    print("recursion")
-    start(READY, request, cont)
-except UnicodeEncodeError:
-    print("unicode")
-    start(READY, request, cont)
+            response = requests.request("GET", url, headers=headers, data=payload)
+            CON_KEY = response.json()["continue"]["apcontinue"]
+            NUM_ITEMS += int(count(response.json()["query"]["allpages"]))
+            print(f'GETTED {count(response.json()["query"]["allpages"])} ITEMS')
+            print(f"TOTAL ITEMS COUNT: {NUM_ITEMS}")
+            time.sleep(2)
+            datas_add = """"""
+            for cat in response.json()["query"]["allpages"]:
+                if not cat["title"] in READY:
+                    datas_add += cat["title"] + "\n"
+                    READY.append(cat["title"])
+                    print(cat["title"])
+            f = open("datas.txt", "a")
+            f.write(datas_add)
+            f.close()
+    except UnicodeDecodeError:
+        print("UNi")
+    except UnicodeEncodeError:
+        print("UNi")
